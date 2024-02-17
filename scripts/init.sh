e422feb62e4c19b0814a7432c00d34384a3c006f
Nasir Hossain <egitsoftnasirhossainnishat@gmail.com>
2024-02-18 00:21:05 +0600

    fix: echo the intro message inside the cttyhack sh
    Doing it this way is better as the clear command is executed after the
    error message "can't access tty" is printed.

--- scripts/init.sh
+++ scripts/init.sh
@@ -11,25 +11,22 @@ mount -t devtmpfs udev /dev
 mount -t proc proc /proc
 mount -t sysfs sysfs /sys
 
-
-# Print the welcome message
-echo "Welcome to the Linux Kernel"
-echo ""
-echo ""
-echo "  ___       ___  ___ ";
-echo " | __|__ _ / _ \/ __|";
-echo " | _|/ _\` | (_) \__ \\";
-echo " |___\__, |\___/|___/";
-echo "     |___/           ";
-echo "Is Built Successfully!";
-echo ""
-echo ""
-echo "Kernel Version: $(uname -r)"
-echo "Kernel Build Date: $(uname -v)"
-echo "Kernel Architecture: $(uname -m)"
-echo "Kernel Compiler: $(uname -o)"
-echo "Kernel Hostname: $(uname -n)"
-
-
 # Start shell with proper tty and power off support
-exec cttyhack sh
+exec cttyhack sh -c '\
+clear\
+echo "Welcome to the Linux Kernel"\
+echo ""\
+echo ""\
+echo "  ___       ___  ___ "\
+echo " | __|__ _ / _ \/ __|"\
+echo " | _|/ _\` | (_) \__ \\"\
+echo " |___\__, |\___/|___/"\
+echo "     |___/           "\
+echo "Less is more easy.  -  EgOS"\
+echo ""\
+echo ""\
+echo "Kernel Version: $(uname -r)"\
+echo "Kernel Build Date: $(uname -v)"\
+echo "Kernel Architecture: $(uname -m)"\
+echo "Kernel Compiler: $(uname -o)"\
+'
